c1927d1fd84f1f4e6248b3c7da1a88d47f9ab013
Alexander Noack <alexander.noack@diva-e.com>
2023-03-14 17:04:27 +0100

    refactor: move extractHttpMethod function

--- src/utilities/http-method.js
+++ src/utilities/http-method.js
@@ -2,3 +2,17 @@ export const HttpMethod = {
   GET: 'get',
   POST: 'post',
 }
+
+/**
+ * @param {object} mapping
+ * @return {HttpMethod}
+ */
+export function extractHttpMethod (mapping) {
+  const supportedMethods = Object.values(HttpMethod)
+
+  const potentialMethod = mapping.split('.').reduce(
+    (_, current, index, array) => index === array.length - 1 ? current : 'none'
+  )
+
+  return supportedMethods.includes(potentialMethod) ? potentialMethod : HttpMethod.GET
+}
